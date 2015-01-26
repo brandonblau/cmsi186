84d5351a1fe5cf93c7b5f143476b9dcd2c749393
Brandon Blau <bblau@lion.lmu.edu>
2015-01-26 15:50:03 -0800

    Create CountdownToArgs.java

---
+++ CountdownToArgs.java
@@ -0,0 +1,21 @@
+//CountdownToArgs.java
+
+public class CountdownToArgs {
+	public static void main(String[] args){
+		try {
+			int maxCount = Integer.parseInt(args[0]);
+			if (maxCount <= 0) {
+				System.out.println("ERROR: " + args[0] + " must be greater than zero");
+			} 
+			for (int i = 0; maxCount >= 0; maxCount = maxCount-1) {
+				System.out.println(maxCount);
+			} 
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("ERROR: No argument provided");
+		
+		} catch (NumberFormatException e) {
+        System.err.println("ERROR: Argument " + args[0] + " must be an integer.");
+        System.exit(1);
+    	}
+	}
+}
